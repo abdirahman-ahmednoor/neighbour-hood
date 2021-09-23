5cf7a66e190a0f485ad91da0e0237da13d790cc0
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-09-23 10:20:31 +0300

    updated admin.py file

--- neighbour/views.py
+++ neighbour/views.py
@@ -7,14 +7,36 @@ from django.contrib.auth.decorators import login_required
 # from .serializer import BusinessSerializer
 from .email import send_amber_email
 from .forms import *
+
 # Create your views here.
 @login_required
 def index(request):
     current_user = request.user
     try:
-        profile = UserProfile.objects.get(user =current_user)
+        profile = UserProfile.objects.get(user = current_user)
     except:
-        return redirect('edit_profile', username = current_user.username)
+        return redirect('edit_profile',username = current_user.username)
 
     try:
-        post = Post.objects    
+        posts = Post.objects.filter(neighborhood = profile.neighborhood)
+    except:
+        posts = None
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.neighborhood = profile.neighborhood
+            post.type = request.POST['type']
+            post.save()
+
+            if post.type == '1':
+                recipients = UserProfile.objects.filter(neighborhood=post.neighborhood)
+                for recipient in recipients:
+                    send_amber_email(post.title,post.content,recipient.email)
+
+        return redirect('index')
+    else:
+        form = PostForm()
+    return render(request,'index.html',{"posts":posts,"profile":profile,"form":form})
