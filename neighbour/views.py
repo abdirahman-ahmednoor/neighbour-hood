285a54710bcfd8059f81ad5a71e8c2ac1f6d083f
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-09-23 10:11:03 +0300

    updated views.py file

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import UserProfile,Post,Neighborhood,Business,Comment
 from django.contrib.auth.decorators import login_required
-from django.http  import HttpResponse
-
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from .serializer import BusinessSerializer
+from .email import send_amber_email
+from .forms import *
 # Create your views here.
 @login_required
 def index(request):
